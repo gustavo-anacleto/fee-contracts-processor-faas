4e0ae214c9b31641568be3fb53512a17650851aa
gustaVASSIO <guguzim.anacleto@gmail.com>
2024-12-01 22:40:43 -0300

    Implement queue verification

--- src/main/java/org/gustavo/LambdaHandler.java
+++ src/main/java/org/gustavo/LambdaHandler.java
@@ -4,10 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
+import java.util.Objects;
+
 public class LambdaHandler implements RequestHandler<SQSEvent, String> {
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
+        if(Objects.isNull(event.getRecords())){
+            return "Queue have no messages";
+        }
+
         event.getRecords().forEach(record -> {
             String messageBody = record.getBody();
             // Processar a mensagem
